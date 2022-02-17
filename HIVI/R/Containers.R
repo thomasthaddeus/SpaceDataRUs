bcdc9374d9daffba3a18b8bd6ffd6b9f94c6a0b6
Thaddeus  Thomas <92204097+thomasthaddeus@users.noreply.github.com>
2022-02-17 12:06:10 -0700

    Change Folder name

---
+++ HIVI/R/Containers.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+read.xlsx("2000-2005(Thad)","2000")
+
+# Aug 2000 Hurricane Debby
+     ("Hurricane Debby\n
+      Aug 2000\n
+      Category={}\n
+      Wind Speed={}")
+Debby <- c()
+
+
+# Aug 2001 – Tropical Storm Dean 
+
+DEAN <- c()
+
+
+# Sept 2004 – Hurricane Jeanne 
+
+Jeanne <- c()
+
+
+# Oct 2008 – Hurricane Omar 
+
+Omar <- c()
+
+
+# Aug 2010 – Hurricane Earl 
+
+Earl <- c()
+
+
+# Oct 2010 – Tropical Storm Otto  
+
+Otto <- c()
+
+
+# Oct 2012 – Hurricane Rafael 
+
+Rafael <- c()
+
+
+# Aug 2015 – Tropical Storm Erika 
+
+Erika <- c()
+
+
+# Sept 2017 – Hurricane Irma 
+
+Irma <- c()
+
+# Sept 2017 – Hurricane Maria 
+
+Maria <- c()
+
+
+# Aug 2019 – Hurricane Dorian 
+
+Dorian <- c()
+
+# Sept 2019 – Tropical Storm Karen 
+
+Karen <- c()
